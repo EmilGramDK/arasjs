a9db714a148fbf11118c46f28225e4e80deecbef
Emil Gram Jensen <emilgram2001@gmail.com>
2025-09-11 14:27:06 +0200

    Add tabs and openSearch method to ArasTabs interface

--- src/types/aras-tabs.ts
+++ src/types/aras-tabs.ts
@@ -1,5 +1,6 @@
 export interface ArasTabs {
   data: Map<string, Record<string, unknown>>;
+  tabs: Array<string>;
   selectTab: (id: string, preventFocus?: boolean) => Promise<boolean>;
   forceCloseAllTabs: () => void;
   closeTabs: () => void;
@@ -8,6 +9,7 @@ export interface ArasTabs {
   setTabContent: (id: string, props: Partial<HeaderTab>) => void;
   addTab: (id: string, options: Partial<HeaderTab>, position?: number) => void;
   open: (src: string, winName: string, isUnfocused: boolean, className?: string) => void;
+  openSearch: (itemTypeId: string, favoriteSearchId?: string) => void;
   getSearchGridTabs: (id: string) => any;
   focusedTab: string;
   selectedTab: string;
